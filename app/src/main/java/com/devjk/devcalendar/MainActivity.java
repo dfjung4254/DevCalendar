eb96bb5b64295c8d40334514e95eb12cc0e0dc0e
dfjung4254 <rmsghk4254@naver.com>
2018-11-01 02:25:47 +0900

    ViewPager, TabLayout 구성.

--- app/src/main/java/com/devjk/devcalendar/MainActivity.java
+++ app/src/main/java/com/devjk/devcalendar/MainActivity.java
@@ -1,10 +1,13 @@
 package com.devjk.devcalendar;
 
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.devjk.devcalendar.fragment.DailyFragment;
 import com.devjk.devcalendar.fragment.MonthlyFragment;
@@ -12,10 +15,40 @@ import com.devjk.devcalendar.fragment.WeeklyFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.MainActivity_Toolbar_toolbar);
+        setSupportActionBar(toolbar);
+
+        tabLayout = (TabLayout) findViewById(R.id.MainActivity_TabLayout_tabLayout);
+        viewPager = (ViewPager) findViewById(R.id.MainActivity_ViewPager_viewPager);
+
+        TabPagerAdapter pagerAdapter = new TabPagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem((tab.getPosition()));
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
     }
 
     private class TabPagerAdapter extends FragmentStatePagerAdapter{
